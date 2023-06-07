f4235c92f1c876deaab9647d1b1b5ffb5e5791cc
Uchirai <uchirai.govere@gmail.com>
2023-06-07 15:06:48 +0200

    first

---
+++ src/Queue.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Queue
+ *
+ * A first-in, first-out data structure
+ */
+class Queue
+{
+
+    /**
+     * Queue items
+     * @var array
+     */
+    protected $items = [];
+
+    /**
+     * Add an item to the end of the queue
+     *
+     * @param mixed $item The item
+     */
+    public function push($item)
+    {
+        $this->items[] = $item;
+    }
+
+    /**
+     * Take an item off the end of the queue
+     *
+     * @return mixed The item
+     */
+    public function pop()
+    {
+        return array_pop($this->items);
+    }
+
+        /**
+     * Take an item off the head of the queue
+     *
+     * @return mixed The item
+     */
+    public function shift()
+    {
+        return array_shift($this->items);
+    }
+
+
+    /**
+     * Get the total number of items in the queue
+     *
+     * @return integer The number of items
+     */
+    public function getCount()
+    {
+        return count($this->items);
+    }
+
+    public function clear()
+    {
+        $this->items = [];
+    }
+}
