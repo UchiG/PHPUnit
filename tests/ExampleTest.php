dba7d1c917d5d65141fbbf47a1e68e087564129f
Uchirai <uchirai.govere@gmail.com>
2023-06-08 22:10:00 +0200

    WeatherMonitorTest

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,11 +1,9 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
 
-class ExampleTest extends TestCase
+class ExampleTest extends MockeryTestCase
 {
-    public function testAddingTwoPlusTwoResultsInFour()
-    {
-        $this->assertEquals(4, 2 + 2);
-    }
+    
 }
