9561c3c5e143590ba2528c794e805169b37f5a25
Uchirai <uchirai.govere@gmail.com>
2023-06-08 22:54:54 +0200

    adjusted the order test

--- tests/OrderTest.php
+++ tests/OrderTest.php
@@ -4,37 +4,39 @@ use PHPUnit\Framework\TestCase;
 
 class OrderTest extends TestCase
 {
-    public function testOrderIsProcessed()
+    public function tearDown(): void
     {
-        $gateway = $this->getMockBuilder('PaymentGateway')
-                        ->setMethods(['charge'])
-                        ->getMock();
-        
-        $gateway->expects($this->once())
-                ->method('charge')
-                ->with($this->equalTo(200))
-                ->willReturn(true);
-        
-        $order = new Order($gateway);
-
-        $order->amount = 200;
-        
-        $this->assertTrue($order->process());
+        Mockery::close();
     }
 
-    public function testOrderIsProcessedUsingMockery()
+    public function testOrderIsProcessedUsingAMock()
     {
-        $gateway = Mockery::mock('PaymentGateway');
+        $order = new Order(3, 1.99);
+        
+        $this->assertEquals(5.97, $order->amount);
 
-        $gateway->shouldReceive('charge')
-                ->once()
-                ->with(200)
-                ->andReturn(true);
-
-        $order = new Order($gateway);
-
-        $order->amount = 200;
-
-        $this->assertTrue($order->process());
+        $gateway_mock = Mockery::mock('PaymentGateway');
+        
+        $gateway_mock->shouldReceive('charge')
+                     ->once()
+                     ->with(5.97);                                     
+                
+        $order->process($gateway_mock);                
     }
+    
+    public function testOrderIsProcessedUsingASpy()
+    {
+        $order = new Order(3, 1.99);
+        
+        $this->assertEquals(5.97, $order->amount);
+
+        $gateway_spy = Mockery::spy('PaymentGateway');
+        
+        $order->process($gateway_spy);
+        
+        $gateway_spy->shouldHaveReceived('charge')
+                    ->once()
+                    ->with(5.97);
+    }    
 }
+
